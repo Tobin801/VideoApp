c359e7a01756c0a0a96c8454dea943930717ea0d
Tobin801 <tand6053@740s-pl-103.local>
2015-11-10 13:35:16 -0700

    I began and added basic coding

---
+++ app/src/main/java/ctec/controller/SoundActivity.java
@@ -0,0 +1,37 @@
+package ctec.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.content.Intent;
+
+public class SoundActivity extends Activity implements Runnable
+{
+    private Button startButton;
+    private Button stopButton;
+    private Button pauseButton;
+    private Button videoButton;
+    private MediaPlayer soundPlayer;
+    private SeekBar soundSeekBar;
+    private Thread soundThread;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        soundSeekBar = (SeekBar) findViewById(R.id.soundSeekBar);
+        videoButton = (Button) findViewById(R.id.videoButton);
+        soundPlayer = MediaPlayer.create(this.getBaseContext(),R.raw.Insert thing here);
+    }
+
+
+}
