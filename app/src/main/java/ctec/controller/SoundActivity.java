dea1ac21313b73549faf9ae3df188dc8fdb5a0fe
Tobin801 <tand6053@740s-pl-103.local>
2015-11-11 13:38:17 -0700

    Fixed a problem in the manifest causing it to not work.

--- app/src/main/java/ctec/controller/SoundActivity.java
+++ app/src/main/java/ctec/controller/SoundActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.*;
 import android.view.View;
-import android.app.Activity;
 import android.media.MediaPlayer;
 import android.content.Intent;
 
